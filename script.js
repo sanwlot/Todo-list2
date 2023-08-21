f19a617909d6b02a1729f276f9ff93e537dfe920
sunnySavlot <123793666+sunnySavlot@users.noreply.github.com>
2023-08-21 21:26:37 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+const todoInput = document.querySelector(".todo-input");
+const todoDateInput = document.querySelector(".todo-date");
+const todoBtn = document.querySelector(".todo-btn");
+const todoListElements = document.querySelector(".todo-list-elements");
+
+const todoList = JSON.parse(localStorage.getItem("todoList")) || []; // get localStorage
+
+renderTodoList();
+
+todoBtn.addEventListener("click", handleAddButtonClick);
+todoInput.addEventListener("keypress", handleKeyPress);
+
+function renderTodoList() {
+  let htmlElements = "";
+  for (let i = 0; i < todoList.length; i++) {
+    htmlElements += `
+        <div>${todoList[i].name}</div> 
+        <div class="date">${todoList[i].dueDate}</div>
+        <button class="delete-button">Delete</button>
+    `;
+  }
+  todoListElements.innerHTML = htmlElements;
+  localStorage.setItem("todoList", JSON.stringify(todoList)); // set localStorage upon updating of todo list
+  deleteTodo();
+}
+
+function handleAddButtonClick() {
+  addTodo();
+}
+
+function handleKeyPress(e) {
+  if (e.key === "Enter") {
+    addTodo();
+  }
+}
+
+function addTodo() {
+  if (todoInput.value !== "") {
+    todoList.push({
+      name: todoInput.value,
+      dueDate: todoDateInput.value,
+    });
+    renderTodoList();
+    todoInput.value = "";
+  }
+}
+
+function deleteTodo() {
+  document.querySelectorAll(".delete-button").forEach((deleteButton, index) => {
+    deleteButton.addEventListener("click", () => {
+      console.log(index)
+      todoList.splice(index, 1);
+      renderTodoList();
+    });
+  });
+}
